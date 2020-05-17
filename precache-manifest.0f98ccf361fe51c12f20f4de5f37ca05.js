c68367cd08dd432d82c98f49a3c6384e5b0a8801
Jhosep Guadalupe <jhosepgc14@gmail.com>
2020-05-16 19:19:02 -0500

    Updates

---
+++ precache-manifest.0f98ccf361fe51c12f20f4de5f37ca05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6befadaf91a3398842a12535a15d5459",
+    "url": "/MiniCarrito/index.html"
+  },
+  {
+    "revision": "b572da0c16f361e0beae",
+    "url": "/MiniCarrito/static/css/main.3ec86de3.chunk.css"
+  },
+  {
+    "revision": "b3c82d0cadf32f1a26d5",
+    "url": "/MiniCarrito/static/js/2.e0307ab2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MiniCarrito/static/js/2.e0307ab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b572da0c16f361e0beae",
+    "url": "/MiniCarrito/static/js/main.1702e24e.chunk.js"
+  },
+  {
+    "revision": "e1af0eb65304da9c8620",
+    "url": "/MiniCarrito/static/js/runtime-main.f036a35d.js"
+  }
+]);
